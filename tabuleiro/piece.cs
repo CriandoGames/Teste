676d8c46c253454381c6a94ff107555867a64421
Carlos <carloscastrogames@gmail.com>
2021-11-13 15:44:52 -0300

    ✨ Feature : Create Class Piece

---
+++ tabuleiro/piece.cs
@@ -0,0 +1,19 @@
+namespace Teste.tabuleiro
+{
+    public class Piece
+    {
+        public Piece(Position position, Color color, int amountOfMovements, Board board)
+        {
+            this.position = position;
+            this.color = color;
+            this.amountOfMovements = 0;
+            this.board = board;
+        }
+
+        public Position position { get; set; }
+    public Color color { get; protected set; }
+    public int amountOfMovements { get; protected set; }
+
+    public Board board { get; protected set; }
+    }
+}
