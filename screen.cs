db056f0c75eb1749f5abe700cec18ec0b6d5d1fc
Carlos <carloscastrogames@gmail.com>
2021-11-15 22:18:28 -0300

    ✨ Feature : Update in Screen Pieces now showColor

--- screen.cs
+++ screen.cs
@@ -7,6 +7,7 @@ namespace Teste.tabuleiro
             Console.Clear();
             for (int i = 0; i < board.rows; i++)
             {
+                 System.Console.Write(8-i + " ");
                 for (int j = 0; j < board.columns; j++)
                 {
                     if (board.piece(i, j) == null)
@@ -15,13 +16,30 @@ namespace Teste.tabuleiro
                     }
                     else
                     {
-                        Console.Write(board.piece(i, j).ToString() + " ");
+                        showColorPiece(board.piece(i, j));
+                        Console.Write(" ");
+                       // Console.Write(board.piece(i, j).ToString() + " ");
                     }
 
                 }
                 System.Console.WriteLine();
             }
+            System.Console.WriteLine("  a b c d e f g h");
+        }
 
+        public static void showColorPiece(Piece piece)
+        {
+            if (piece.color == Color.branca)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux  = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
